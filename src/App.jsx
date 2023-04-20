0d39b79bfc855256afb3a8ab533950d6c30d24f1
Lucas Speziale <103441307+lucas271102@users.noreply.github.com>
2023-04-20 18:57:00 -0300

    minga amarillo (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import img1 from './assets/img1.png'
+import img2 from './assets/img2.png'
+import NAV from './assets/NAV.png'
+import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
-let title= 'Welcome to Mindhub'
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header className="bg-background">
+      <nav className='p-8 flex'>
+            <img src={NAV} alt="" srcset="" />
+      </nav>
+           <h1 className='text-5xl text-white font-bold flex justify-content:center; p-9'>For the love of manga</h1>
+    </header>
+    
+     <div className='bg-stone-700'>
+      <div className='bg-orange-500 flex justify-center; p-10 text-white'>
+        <img src={img1} alt="" srcset="" />
+          <img src={img2} alt="" srcset="" />
+          <div>
+            <h1 className='text-xl font-medium flex justify-center'>Shonen</h1>
+            <p className='flex justify-center'>Is the manga that is aimed at adolescent boys.
+              They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+          </div>
+          
       </div>
-      <h1>{title}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
