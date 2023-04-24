14f666c51c3633b0d1ab2d7563cd3b6969d9af82
Lucas Speziale <103441307+lucas271102@users.noreply.github.com>
2023-04-24 00:33:08 -0300

    F 01 (#2)
    * minga amarillo
    
    * home

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,8 @@ module.exports = {
   ],
   theme:
    {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
+
+
   extend: {
     colors:{
       primary:"#222"
