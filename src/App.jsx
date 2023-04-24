1f383499af843876382da549123c9baae1161d68
lucas271102 <lucas001spe@gmail.com>
2023-04-24 15:48:36 -0300

    listo

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Twitter from'./assets/Twitter.png'
 import Vimeo from'./assets/Vimeo.png'
 import Youtube from'./assets/Youtube.png'
 import footerbg from './assets/footerbg.png'
+import circlearrow from './assets/circlearrow.png'
+import circle from './assets/circle.png'
 
 import './index.css'
 
@@ -20,7 +22,7 @@ console.log(process.env.NODE_ENV)
   return (
     <>
 
-    <header className="bg-background w-full object-cover   h-80vh w-full  " >
+    <header className="bg-background bg-no-repeat bg-cover w-full object-cover h-[60vh]    w-full  " >
       <nav className=' flex gap-12 p-6 min-[640px]:space-x-96 gap-x-96 flex-grow'>
       <button><img  src={Menu} alt="" /></button>
             <img src={Logo} alt="" srcset=""   />
@@ -39,15 +41,22 @@ console.log(process.env.NODE_ENV)
     
      <div className=' hidden min-[640px]:block bg-white-700 p-20 flex justify-center '>
       <div className='bg-gradient-to-r from-orange-500 to-orange-600  min-[640px]:flex justify-center; h-64 p-6 text-white space-x-36 w-80vh rounded-lg'>
-        <img src={img1} alt="" srcset="" className='mb-28 h-60 '/>
-          <img   src={img2} alt="" srcset="" className=' left-96 bottom-96 ' />
+        <div className='flex justify-center items-center'>
+        <img src={circlearrow} className='w-6 h-6 flex items-center justify-center' alt="" srcset="" />
+        </div>
+       
+        <img src={img1} alt="" srcset="" className='mt-[-3rem] h-60 '/>
+          <img   src={img2} alt="" srcset="" className='mt-[-3rem] h-60 ' />
           <div className='w-1/3 p-9 text-left left-96'>
             
             <h1 className='text-xl font-medium '>Shonen</h1>
              <p className='flex justify-center  leading-4'>Is the manga that is aimed at adolescent boys.
               They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
           </div>
-          
+          <div className='flex items-center justify-center'>
+          <img src={circle} className='w-6 h-6 flex items-center justify-center' alt="" srcset="" />
+          </div>
+        
       </div>
      </div>
      <div>
