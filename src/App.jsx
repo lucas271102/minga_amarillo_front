a1ff9a045b249dee9048332e878dbf83ab7c3787
Lucas Speziale <103441307+lucas271102@users.noreply.github.com>
2023-04-24 16:57:47 -0300

    components (#4)

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import img2 from './assets/img2.png'
 import apiUrl from '../api'
 import Logo from './assets/Logo.png'
 import Menu from './assets/Menu.png'
-import Facebook from'./assets/Facebook.png'
-import Twitter from'./assets/Twitter.png'
-import Vimeo from'./assets/Vimeo.png'
-import Youtube from'./assets/Youtube.png'
-import footerbg from './assets/footerbg.png'
+import Carousel from './components/Carousel.jsx'
+
 import circlearrow from './assets/circlearrow.png'
 import circle from './assets/circle.png'
+import Footer from './components/Footer.jsx'
 
 import './index.css'
 
@@ -39,48 +37,9 @@ console.log(process.env.NODE_ENV)
           
     </header>
     
-     <div className=' hidden min-[640px]:block bg-white-700 p-20 flex justify-center '>
-      <div className='bg-gradient-to-r from-orange-500 to-orange-600  min-[640px]:flex justify-center; h-64 p-6 text-white space-x-36 w-80vh rounded-lg'>
-        <div className='flex justify-center items-center'>
-        <img src={circlearrow} className='w-6 h-6 flex items-center justify-center' alt="" srcset="" />
-        </div>
-       
-        <img src={img1} alt="" srcset="" className='mt-[-3rem] h-60 '/>
-          <img   src={img2} alt="" srcset="" className='mt-[-3rem] h-60 ' />
-          <div className='w-1/3 p-9 text-left left-96'>
-            
-            <h1 className='text-xl font-medium '>Shonen</h1>
-             <p className='flex justify-center  leading-4'>Is the manga that is aimed at adolescent boys.
-              They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
-          </div>
-          <div className='flex items-center justify-center'>
-          <img src={circle} className='w-6 h-6 flex items-center justify-center' alt="" srcset="" />
-          </div>
-        
-      </div>
-     </div>
+   <Carousel/>
      <div>
-     <footer className='bg-white-700 flex flex-col'>
-  <div >
-    <div className='w-full'>
-      <img src={footerbg} alt="" className='rounded-[60%_60%_100%100%/_0%_0%_100%_100%] h-44 min-[640px]:w-screen h-56'/>
-    </div>
-    <div className='w-screen md:w-2/3 flex flex-col container mx-auto flex flex-col justify-end items-center min-[640px]:flex-row p-9 justify-center space-x-12'>
-      <h2 className='text-black text-center md:text-left mb-4 md:mb-0 mx-4 md:mx-0'>Home</h2>
-      <h2 className='text-black text-center md:text-left mb-4 md:mb-0 mx-4 md:mx-0'>Mangas</h2>
-      <h2 className='text-orange-600 text-center md:text-left text-2xl font-semibold mb-4 md:mb-0 mx-4 md:mx-0'>Minga雪</h2>
-    <div >
-      <div className='flex gap-x-12 bottom-3'>
-      <img src={Facebook} alt="" />
-      <img src={Twitter} alt="" />
-      <img src={Vimeo} alt="" />
-      <img src={Youtube} alt="" />
-      </div>
-      <button className='bg-orange-600 text-white px-4 py-2 rounded-md mx-4 md:mx-0 w-60'>Donate</button>
-    </div>
-    </div>
-  </div>
-</footer>
+    <Footer/>
 </div>
 
   
---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+
+import Facebook from '../assets/Facebook.png'
+import Twitter from'../assets/Twitter.png'
+import Vimeo from'../assets/Vimeo.png'
+import Youtube from'../assets/Youtube.png'
+import footerbg from '../assets/footerbg.png'
+
+export default function Footer () {
+  return (
+    <div>
+         <footer className='bg-white-700 flex flex-col'>
+  <div >
+    <div className='w-full'>
+      <img src={footerbg} alt="" className='rounded-[60%_60%_100%100%/_0%_0%_100%_100%] h-44 min-[640px]:w-screen h-56'/>
+    </div>
+    <div className='w-screen md:w-2/3 flex flex-col container mx-auto flex flex-col justify-end items-center min-[640px]:flex-row p-9 justify-center space-x-12'>
+      <h2 className='text-black text-center md:text-left mb-4 md:mb-0 mx-4 md:mx-0'>Home</h2>
+      <h2 className='text-black text-center md:text-left mb-4 md:mb-0 mx-4 md:mx-0'>Mangas</h2>
+      <h2 className='text-orange-600 text-center md:text-left text-2xl font-semibold mb-4 md:mb-0 mx-4 md:mx-0'>Minga雪</h2>
+    <div >
+      <div className='flex gap-x-12 bottom-3'>
+      <img src={Facebook} alt="" />
+      <img src={Twitter} alt="" />
+      <img src={Vimeo} alt="" />
+      <img src={Youtube} alt="" />
+      </div>
+      <button className='bg-orange-600 text-white px-4 py-2 rounded-md mx-4 md:mx-0 w-60'>Donate</button>
+    </div>
+    </div>
+  </div>
+</footer>
+    </div>
+  )
+}
+ 
