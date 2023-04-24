06454094084e1e8816ef026894a9c17629e89788
lucas271102 <lucas001spe@gmail.com>
2023-04-24 13:48:31 -0300

    m-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import img1 from './assets/img1.png'
 import img2 from './assets/img2.png'
-
+import apiUrl from '../api'
 import Logo from './assets/Logo.png'
 import Menu from './assets/Menu.png'
 import Facebook from'./assets/Facebook.png'
@@ -15,7 +15,8 @@ import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+console.log(apiUrl)
+console.log(process.env.NODE_ENV)
   return (
     <>
 
@@ -71,30 +72,11 @@ function App() {
     </div>
   </div>
 </footer>
+</div>
 
-=======
-    <header className="bg-background">
-      <nav className='p-8 flex'>
-            <img src={NAV} alt="" srcset="" />
-      </nav>
-           <h1 className='text-5xl text-white font-bold flex justify-content:center; p-9'>For the love of manga</h1>
-    </header>
-    
-     <div className='bg-stone-700'>
-      <div className='bg-orange-500 flex justify-center; p-10 text-white'>
-        <img src={img1} alt="" srcset="" />
-          <img src={img2} alt="" srcset="" />
-          <div>
-            <h1 className='text-xl font-medium flex justify-center'>Shonen</h1>
-            <p className='flex justify-center'>Is the manga that is aimed at adolescent boys.
-              They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
-          </div>
-          
-      </div>
+  
+  
+</>
 
-     </div>
-    </>
-  )
-}
 
-export default App
+  );}export default App 
