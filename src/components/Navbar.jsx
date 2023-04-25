3aec005a8160a4082f191eb6d8da00598858993e
Lucas Speziale <103441307+lucas271102@users.noreply.github.com>
2023-04-25 00:37:22 -0300

    f-03 (#5)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Menu from '../assets/Menu.png'
+import Logo from '../assets/Logo.png'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+
+<header className="bg-background bg-no-repeat bg-cover w-full object-cover h-[60vh]    w-full  " >
+      <nav className=' flex gap-12 p-6 min-[640px]:space-x-96 gap-x-96 flex-grow'>
+      <button><img  src={Menu} alt="" /></button>
+            <img src={Logo} alt="" srcset=""   />
+            
+      </nav>
+          
+         
+          
+    </header>
+    </>
   )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,21 @@
+
+import Button from "../components/Button.jsx"
+export default function () {
+  return (
+    <>
+   <div className='relative top-[-20%] xl:bottom-48 sm:bottom-16'>
+  <div className='absolute  h-full w-full'>
+    <div className='h-full flex justify-center items-center sm:h-[82%] border border-black'>
+      <div className='mb-40 w-scren items-center flex flex-col sm:mb-20 sm:w-5/6 sm:items-start'>
+        <h1 className='text-white font-bold text-3xl w-[80%] text-center sm:w-[70%] sm:text-left xl:text-6xl'>For the love of manga</h1>
+        <h3 className='text-white font-semibold p-2 text-center sm:font-normal xl:text-2xl'>Explore our varieties</h3>
+        <h4 className='text-white hidden font-semibold sm:block xl:text-1xl'>#MingaLove❤</h4>
+       <Button/>
+      </div>
+    </div>
+  </div>
+</div>
+   
+    </>
+  )
+}
